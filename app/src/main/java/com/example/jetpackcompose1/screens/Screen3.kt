aef4be2bd2169727129067d689539682e2ad6c82
angel <jjjk>
2022-12-15 16:01:03 +0100

    Funciona.

---
+++ app/src/main/java/com/example/jetpackcompose1/screens/Screen3.kt
@@ -0,0 +1,11 @@
+package com.example.jetpackcompose1.screens
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun Screen3(){
+    Text(text = "Esta la creé yo!")
+
+}
