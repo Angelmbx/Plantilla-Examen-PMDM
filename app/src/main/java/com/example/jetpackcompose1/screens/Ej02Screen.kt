b3069fc6390cafb80bcc8c1a91ab2b4b1ac071c2
a21angelmb <email>
2022-12-15 19:15:16 +0100

    wip

--- app/src/main/java/com/example/jetpackcompose1/screens/Ej02Screen.kt
+++ app/src/main/java/com/example/jetpackcompose1/screens/Ej02Screen.kt
@@ -1,9 +1,9 @@
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Flag
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -12,21 +12,39 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 
+val spacedByDefault = 5.dp
+const val boardSize = 8
+
+
 @Preview
 @Composable
 fun Screen2() {
-    Box(  // (1)
-        Modifier
-            .background(Color.Yellow)
-            .fillMaxSize()
-            .padding(30.dp)
-            .background(Color.Green),
-        // (1)
-        content = {
-            Text(text = "Texto 1 Hadcodeado", Modifier.align(Alignment.CenterEnd))
-            Text(text = "Otro texto hardcodeado", Modifier.align(Alignment.BottomEnd))
-            Text(text = "Texto 4 Hadcodeado", Modifier.align(Alignment.TopCenter))
+    Column(
+        Modifier.fillMaxSize().padding(10.dp),
+        verticalArrangement = Arrangement.spacedBy( // (1)
+            space = spacedByDefault,
+            alignment = Alignment.CenterVertically
+        ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        repeat(boardSize) {
+            Row(horizontalArrangement = Arrangement.spacedBy(spacedByDefault)) {
+                repeat(boardSize) {
+                    Button(
+                        onClick = { /*TODO*/ },
+                        Modifier
+                            .weight(1f)
+                            .aspectRatio(1f)  // (2)
+                    ) {
+                        Icon(
+                            Icons.Default.Flag,
+                            contentDescription = null,
+                        )
+                    }
+                }
+            }
         }
-    )
+    }
+}
 
 }
